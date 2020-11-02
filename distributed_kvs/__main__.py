1a7c22d17e0cde58f456e51ee354f0a7cf049998
Michael Covarrubias <mdcovarrubias@gmail.com>
2020-11-02 10:46:14 -0800

    create api from assignment 1 in flask

--- distributed_kvs/__main__.py
+++ distributed_kvs/__main__.py
@@ -1,7 +1,23 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+@app.route('/', methods=['GET', 'POST'])
+def index():
     return 'Hello, World!'
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    return 'Hello, World!'
+
+@app.route('/hello/<string:name>', methods=['POST'])
+def hello_name(name):
+    return 'Hello, {0}!'.format(name)
+
+@app.route('/echo/<string:msg>', methods=['GET', 'POST'])
+def echo(msg):
+    if request.method =='POST':
+        return 'POST message reveived: {0}'.format(msg)
+    if request.method == 'GET':
+        return 'This method is unsupported.', 405
