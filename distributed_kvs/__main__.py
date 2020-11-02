eb7fb88227a015362024a088f6605c69a9bcce5d
Michael Covarrubias <mdcovarrubias@gmail.com>
2020-11-02 12:03:25 -0800

    fix typo's in responses

--- distributed_kvs/__main__.py
+++ distributed_kvs/__main__.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'Hello, World!'
+    return 'Hello, world!'
 
 @app.route('/hello', methods=['GET'])
 def hello():
-    return 'Hello, World!'
+    return 'Hello, world!'
 
 @app.route('/hello/<string:name>', methods=['POST'])
 def hello_name(name):
@@ -18,9 +18,9 @@ def hello_name(name):
 @app.route('/echo/<string:msg>', methods=['GET', 'POST'])
 def echo(msg):
     if request.method =='POST':
-        return 'POST message reveived: {0}'.format(msg)
+        return 'POST message received: {0}'.format(msg)
     if request.method == 'GET':
         return 'This method is unsupported.', 405
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8081, debug=True)
+    app.run(host='localhost', port=8088)
