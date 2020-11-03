0cf25a2a13a81250caeece838727c79b99df4a7b
Michael Covarrubias <mdcovarrubias@gmail.com>
2020-11-02 19:39:07 -0800

    Add logic to handle PUT requests for path kvs

--- distributed_kvs/__main__.py
+++ distributed_kvs/__main__.py
@@ -1,5 +1,30 @@
 from flask import Flask
 from flask import request
+from flask import jsonify
+
+kv_store = {}
+KEY_LENGTH = 50
+ADDED_MESSAGE = 'Added successfully'
+UPDATED_MESSAGE = 'Updated successfully'
+MISSING_RESPONSE = {
+    "error": "Value is missing",
+    "message": "Error in PUT"
+}
+
+BAD_FORMAT_RESPONSE = {
+    "error": "Bad json format",
+    "message": "Error in PUT"
+}
+
+ADD_AND_REPLACE_RESPONSE = {
+    "message": "Added successfully",
+    "replaced": False
+}
+
+LONG_KEY_RESPONSE = {
+    "error": "Key is too long",
+    "message": "Error in PUT"
+}
 
 app = Flask(__name__)
 
@@ -22,5 +47,45 @@ def echo(msg):
     if request.method == 'GET':
         return 'This method is unsupported.', 405
 
+@app.route('/kvs/<string:key>', methods=['GET', 'PUT', 'DELETE'])
+def kvs(key):
+    if request.method == 'PUT':
+        response = {}
+        code = None
+
+        try:
+            content = request.get_json()
+        except:
+            # Error: Invalid Json format
+            return jsonify(BAD_FORMAT_RESPONSE), 400
+
+        try:
+            value = content['value']
+        except:
+            # Error: Key value did not exist
+            return jsonify(MISSING_RESPONSE), 400
+
+        if len(key) > KEY_LENGTH:
+            return jsonify(LONG_KEY_RESPONSE), 400
+
+        # at this point we have a valid value and key
+        if key in kv_store:
+            replace = True
+            message = UPDATED_MESSAGE
+            code = 200
+        else:
+            replace = False
+            message = ADDED_MESSAGE
+            code = 201
+
+        response['replace'] = replace
+        response['message'] = message
+
+        kv_store[key] = value
+
+        return jsonify(response), 201
+
+    return 'good'
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8081)
+    app.run(host='localhost', port=13800)
