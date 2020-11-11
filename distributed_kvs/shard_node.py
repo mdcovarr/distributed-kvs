05135b91afa73c2bfc7b3e7a5d90bc64942410f6
Michael Covarrubias <mdcovarrubias@gmail.com>
2020-11-11 15:07:48 -0800

    Added a shard node wrapper class
    This class will be used to wrapp all variables and functions
    needed for the flask app to operate

--- distributed_kvs/shard_node.py
+++ distributed_kvs/shard_node.py
@@ -10,30 +10,60 @@ import os
 kv_store = {}
 
 app = Flask(__name__)
+class ShardNodeWrapper(object):
+    """
+        Class object to wrapp around Flask server and
+        needed variables e.g., key-value store
+    """
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.kv_store = {}
+        self.views = {}
+        self.address = ''
 
-@app.route('/kvs/key-count', methods=['GET'])
-def key_count():
-    """
-    Method used to handle getting key count of
-    distributed key-value store
-    :return count: the total count of keys in the store
-    """
-    return 'Hello, world!'
+    def setup_routes(self):
+        """
+        Method used to set up the url rules for the Flask app
+            /kvs/keys/<key>
+            /kvs/key-count
+            /kvs/view-change
+        :return None:
+        """
+        self.app.add_url_rule(
+                rule='/kvs/key-count', endpoint='key_count', view_func=self.key_count, methods=['GET'])
+        self.app.add_url_rule(
+                rule='/kvs/view-change', endpoint='view_change', view_func=self.view_change, methods=['PUT'])
+        self.app.add_url_rule(
+                rule='/kvs/keys/<string:key>', endpoint='keys', view_func=self.keys, methods=['GET', 'PUT', 'DELETE'])
 
-@app.route('/kvs/view-change', methods=['PUT'])
-def view_change():
-    """
-    Method used to handle the changing of a view
-    in the distributed key-value store
-    :return status: the status of the HTTP PUT request
-    """
-    return 'Hello, world!'
+    def run(self):
+        """
+        Method to start flask server
+        :return None:
+        """
+        self.app.run(host='127.0.0.1', port=13800)
 
-@app.route('/kvs/keys/<string:key>', methods=['GET', 'PUT', 'DELETE'])
-def keys(key):
-    """
-    Method used to handle the GET, PUT, or DELETE of a key
-    in the distributed key-value store
-    :param key: the key of interest
-    :return status: the response of the given HTTP request
-    """
+    def key_count(self):
+        """
+        Method used to handle getting key count of
+        distributed key-value store
+        :return count: the total count of keys in the store
+        """
+        return 'Hello, world!'
+
+    def view_change(self):
+        """
+        Method used to handle the changing of a view
+        in the distributed key-value store
+        :return status: the status of the HTTP PUT request
+        """
+        return 'Hello, world!'
+
+    def keys(self, key):
+        """
+        Method used to handle the GET, PUT, or DELETE of a key
+        in the distributed key-value store
+        :param key: the key of interest
+        :return status: the response of the given HTTP request
+        """
+        return 'Hello, world!'
