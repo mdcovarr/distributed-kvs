bd4f2809daad50bdeb3908dd5fd03ec4773a36e7
Michael Covarrubias <mdcovarrubias@gmail.com>
2020-11-11 11:23:29 -0800

    Add documentation to app.py
    app.py is responsible for starting off the correct server.
    Either a proxy or a main server

--- distributed_kvs/app.py
+++ distributed_kvs/app.py
@@ -1,3 +1,11 @@
+"""
+    Main entrance of REST Server
+
+    Need to determine if there is a FORWARDING_ADDRESS value in the
+    environment. If yes, we start a 'follower' or 'proxy' node.
+    If no, we start a 'main' node.
+"""
+
 from flask import Flask
 from flask import request
 from flask import jsonify
@@ -6,9 +14,14 @@ from main_app import main_blueprint
 from proxy_app import proxy_blueprint
 
 if __name__ == '__main__':
+    """
+        Code main entrance
+    """
     app = Flask(__name__)
 
+
     FORWARDING_ADDRESS = os.environ.get('FORWARDING_ADDRESS')
+
     if FORWARDING_ADDRESS and FORWARDING_ADDRESS != '':
         app.register_blueprint(proxy_blueprint)
     else:
